8590e3e88d5739fdb9b67122d3825d776e3b99ad
julianoks <juao15@hampshire.edu>
2018-12-30 10:58:19 -0500

    mini lisp

---
+++ mini_lisp/lisp.js
@@ -0,0 +1,137 @@
+function parseProgram(str){
+    let expression = ['']
+    let stack = [expression]
+    for(const char of str){
+        let context = stack[stack.length-1]
+        const lastExp = context[context.length-1]
+        if(char === '('){
+            if(lastExp === ''){ context.pop() }
+            let next = ['']
+            context.push(next)
+            stack.push(next)
+        } else if(char === ')'){
+            if(lastExp === ''){ context.pop() }
+            stack.pop()
+        } else if(/\s/g.test(char)){
+            if(lastExp !== ''){
+                context.push('')
+            }
+        } else {
+            context[context.length-1] += char
+        }
+    }
+    return expression
+}
+
+function bind(ks, vs){
+	let acc = {}
+	for(const i in ks){
+		if(ks[i].startsWith('...')){
+			return Object.assign(acc, {[ks[i].slice(3)]: vs.slice(i)})
+		} else { acc[ks[i]] = vs[i] }
+	}
+	return acc
+}
+
+function destructure(args, env){
+    let acc = []
+    for(let i=0; i<args.length; i++){
+        if(typeof(args[i]) === 'string' && args[i].startsWith('...')){
+            if(args[i] === '...'){
+                i++
+                acc.push(...args[i])
+            } else {
+                acc.push(...evalExp(args[i].slice(3), env))
+            }
+        } else { acc.push(args[i]) }
+    }
+    for(const v of args){
+       
+    }
+    return acc
+}
+
+const baseOps = {
+    'def': ([key, val], env) => { return env[key] = evalExp(val, env) },
+    'if': ([cond, a, b], env) => evalExp(evalExp(cond, env)? a : b, env),
+    'quote': (args) => args[0],
+    'fn': ([params, ...exprs], env) => (...supplied) => {
+        const newEnv = Object.assign({}, env, bind(params, supplied))
+        return exprs.map(a => evalExp(a, newEnv)).pop()
+    },
+    'macro': ([params, ...exprs], env) => (f => {f.isMacro=true; return f;})(
+        (...supplied) => {
+            const subEnv = Object.assign({}, env, bind(params, supplied))
+            const transformed = exprs.map(a => evalExp(a, subEnv)).pop()
+            return evalExp(transformed, env)
+        })
+}
+
+function evalExp(exp, env){
+    if(!isNaN(+exp)) return +exp
+    if(typeof(exp) === 'string' && exp.startsWith("'")) return exp.slice(1)
+    if(typeof(exp) === 'string') return env[exp]
+    if(!Array.isArray(exp)) return exp
+    const [op, ...args] = destructure(exp, env)
+    if(typeof(op) === 'string' && baseOps.hasOwnProperty(op)){
+        return baseOps[op](args, env)
+    }
+    const f = evalExp(op, env)
+    return f(...(f.isMacro? args : args.map(a => evalExp(a, env))))
+}
+
+const newEnvironment = () => {
+    let env = {
+        // lists
+        'list': (...args) => args,
+        'count': (list) => list.length,
+        'first': (list) => list[0],
+        'rest': (list) => list.slice(1),
+        'last': (list) => list[list.length-1],
+        'cons': (ele, list) => [ele, ...list],
+        'conj': (ele, list) => [...list, ele],
+        'nth': (list, n) => list[n],
+        // higher order
+        'map': (fn, list) => list.map(fn),
+        'filter': (fn, list) => list.filter(fn),
+        'reduce': (fn, list, init=null) => list.reduce(fn, init),
+        'apply': (fn, list) => fn(...list),
+        // boolean
+        'true': true,
+        'false': false,
+        'and': (...exprs) => exprs.reduce((a,b) => a && b, true),
+        'or': (...exprs) => exprs.reduce((a,b) => a || b, true),
+        'not': (x) => !x,
+        // math
+        '+': (...args) => args.reduce((a,b) => a+b),
+        '*': (...args) => args.reduce((a,b) => a*b),
+        '-': (...args) => args.reduce((a,b) => a-b),
+        '/': (...args) => args.reduce((a,b) => a/b),
+        '>': (a,b) => a>b, '<': (a,b) => a<b,'=': (a,b) => a==b,
+        // etc
+        'get': (obj, prop) => obj[prop],
+        'range': (n) => Array(n).fill().map((_,i)=>i),
+        'console': console.log,
+        'window': window,
+        'Math': Math,
+    }
+    const stdMacros = `
+    (def defn
+        (macro (name args body)
+            (list 'def name
+                (list 'fn args body))))
+
+    (def defmacro
+        (macro (name args body)
+            (list 'def name
+                (list 'macro args body))))
+    `
+    evalProgram(stdMacros, env)
+    return env
+}
+
+let GLOBAL = newEnvironment()
+
+export function evalProgram(str, env=GLOBAL){
+    return parseProgram(str).map(p => evalExp(p, env)).pop()
+}
---
+++ mini_lisp/repl.js
@@ -0,0 +1,38 @@
+import {evalProgram} from './lisp.js'
+
+const defaultSubmitAction = (inputValue, textDiv) => {
+    textDiv.innerText += `>>> ${inputValue}`
+    textDiv.scrollTop = textDiv.scrollHeight
+    try{
+        textDiv.innerText += `\n${evalProgram(inputValue)}\n`
+    } catch(e){
+        textDiv.innerText += `\n${e}\n`
+    }
+    textDiv.scrollTop = textDiv.scrollHeight
+} 
+
+export const newDOM = (submitAction=defaultSubmitAction) => {
+    const holder = document.createElement('div')
+    const textDiv = document.createElement('div')
+    const input = document.createElement('textarea')
+
+    holder.appendChild(textDiv)
+    holder.appendChild(input)
+
+    holder.style.width = '100%'
+    holder.style.height = '50%'
+    textDiv.style = 'height: 100%; width: 100%; overflow: scroll; border: 1px solid;'
+    input.setAttribute('rows', 2)
+    input.style.width = '100%'
+
+    input.addEventListener('keydown', e => {
+        if(e.keyCode === 13 && !e.shiftKey){
+            e.preventDefault()
+            submitAction(input.value, textDiv)
+            input.value = ''
+        }
+    })
+
+    return holder
+}
+
